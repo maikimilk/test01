bdd3ac64ad46280c9df92fa7c3c7df286a6c35a4
maikimilk <maimu.ltd@gmail.com>
2021-05-12 11:51:43 +0900

    振動するプログラムを追加した

---
+++ vi.js
@@ -0,0 +1,9 @@
+let element = document.getElementById('button');
+console.log(element)
+element.addEventListener('click', clickFunction);
+
+function clickFunction() {
+    console.log("クリックされました");
+    navigator.vibrate(100);
+}
+
